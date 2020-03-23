106e824c4c27d90cc771e2fa49d2b26b80880e8f
Алексей Андрюшин <aleksroebuck@gmail.com>
2020-03-23 13:05:33 +0200

    Добавьте файлы проекта.

---
+++ Web-ProgrammingMVC/Controllers/CarsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IActionResult List(string name)
+        {
+            ViewBag.Message = "Ваше ім'я " + name;
+            return View();
+        }
+    }
+}
---
+++ Web-ProgrammingMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using Shop.Models;
+
+namespace Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
